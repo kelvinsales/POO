400262822da8e28d6d4daf919bf555c034b3f925
kelvin <kelvin@kelvin-casa>
2023-11-07 17:07:52 -0300

    ALterado novamente

--- poo/src/main/java/ifce/poo/Poo.java
+++ poo/src/main/java/ifce/poo/Poo.java
@@ -4,6 +4,8 @@
 
 package ifce.poo;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author kelvin
@@ -12,5 +14,6 @@ public class Poo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        JOptionPane.showMessageDialog(null, "OI Pessoal!");
     }
 }
