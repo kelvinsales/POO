da31f1089a39623fed4597f68e1690661f7c1ca0
kelvin <kelvin@kelvin-casa>
2023-11-07 16:35:03 -0300

    Iniciei projeto novo

---
+++ poo/src/main/java/ifce/poo/Poo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ifce.poo;
+
+/**
+ *
+ * @author kelvin
+ */
+public class Poo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
