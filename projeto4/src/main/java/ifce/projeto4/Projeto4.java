d29f364c3df63c088b0a6d31cc27f6207b884ef5
kelvin <kelvin@kelvin-casa>
2023-11-21 15:26:26 -0300

    Conversor Velocidade

---
+++ projeto4/src/main/java/ifce/projeto4/Projeto4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ifce.projeto4;
+
+/**
+ *
+ * @author kelvin
+ */
+public class Projeto4 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ projeto4/src/main/java/modelo/ConversorVelo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author kelvin
+ */
+public class ConversorVelo {
+    //ATRIBUTOS
+    private double veloMs;
+    private double veloKmh;
+    //CONSTRUTOR
+    
+    public ConversorVelo(){
+        
+    }   
+    //METODOS
+    public double converterVelo(double v, String tipo){
+        return veloKmh;
+    }
+}
